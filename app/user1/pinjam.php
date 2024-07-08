d5ff6ba509b48d81c75f282c6e4783260fa77161
4zan4 <fauzanadityaputra60@gmail.com>
2024-07-08 22:37:53 +0700

    fix

--- app/user1/pinjam.php
+++ app/user1/pinjam.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-// Pastikan pengguna sudah login sebelum mengakses halaman ini
+// Ensure the user is logged in before accessing this page
 if (!isset($_SESSION['username'])) {
     header("Location: ../../function/Process.php");
     exit();
@@ -9,10 +9,10 @@ if (!isset($_SESSION['username'])) {
 
 include "../../config/koneksi.php";
 
-// Ambil ID pengguna dari sesi
+// Get the user ID from the session
 $id_user = $_SESSION['id_user'];
 
-// Query untuk mengambil daftar buku yang dipinjam oleh pengguna
+// Query to get the list of books borrowed by the user
 $query = "SELECT buku.*, peminjaman.tanggal_pinjam, peminjaman.tanggal_kembali
           FROM peminjaman
           INNER JOIN buku ON peminjaman.id_buku = buku.id_buku
@@ -22,7 +22,7 @@ $stmt->bind_param("i", $id_user);
 $stmt->execute();
 $result = $stmt->get_result();
 
-// Inisialisasi array untuk menyimpan buku yang dipinjam
+// Initialize array to store borrowed books
 $buku_dipinjam = [];
 
 while ($row = $result->fetch_assoc()) {
@@ -31,7 +31,6 @@ while ($row = $result->fetch_assoc()) {
 
 $stmt->close();
 $koneksi->close();
-
 ?>
 
 <!DOCTYPE html>
@@ -50,16 +49,17 @@ $koneksi->close();
         }
     </style>
 
-    <?php @include('head.php'); ?>
+    <?php include('head.php'); ?>
+</head>
 
 <body data-bs-spy="scroll" data-bs-target="#header" tabindex="0">
 
-    <?php @include('navbar.php'); ?>
+    <?php include('navbar.php'); ?>
+
     <section id="popular-books" class="bookshelf py-5 my-5">
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-
                     <div class="section-header align-center">
                         <div class="title">
                             <span>Some quality items</span>
@@ -73,50 +73,50 @@ $koneksi->close();
                     <div class="tab-content">
                         <div id="Dipinjam" data-tab-content class="active">
                             <div class="row" id="book-list">
-                                <?php while ($book = $resultDipinjam->fetch_assoc()) : ?>
+                                <?php foreach ($buku_dipinjam as $book) : ?>
                                     <div class="col-md-3">
                                         <div class="product-item">
                                             <figure class="product-style">
-                                                <img src="<?= $book['img']; ?>" alt="Books" class="product-item">
+                                                <img src="<?= htmlspecialchars($book['img']); ?>" alt="Books" class="book-image">
                                             </figure>
                                             <figcaption>
-                                                <h3><?= $book['judul_buku']; ?></h3>
-                                                <span>Status: <?= $book['status']; ?></span>
-                                                <span>Tanggal Pinjam: <?= $book['tanggal_pinjam']; ?></span>
-                                                <p><span>Harus Dikembalikan Sebelum <?= $book['tanggal_kembali']; ?></span></p>
+                                                <h3><?= htmlspecialchars($book['judul_buku']); ?></h3>
+                                                <span>Status: <?= htmlspecialchars($book['status']); ?></span>
+                                                <span>Tanggal Pinjam: <?= htmlspecialchars($book['tanggal_pinjam']); ?></span>
+                                                <p><span>Harus Dikembalikan Sebelum <?= htmlspecialchars($book['tanggal_kembali']); ?></span></p>
                                                 <div class="btn-wrap">
-                                                    <a href="#" class="btn-accent-arrow kembalikan-btn" data-bs-toggle="modal" data-bs-target="#modalKembalikan<?= $book['id_buku']; ?>">Kembalikan <i class="icon icon-ns-arrow-right"></i></a>
+                                                    <a href="#" class="btn-accent-arrow kembalikan-btn" data-bs-toggle="modal" data-bs-target="#modalKembalikan<?= htmlspecialchars($book['id_buku']); ?>">Kembalikan <i class="icon icon-ns-arrow-right"></i></a>
                                                 </div>
-                                                <div class="modal fade" id="modalKembalikan<?= $book['id_buku']; ?>" tabindex="-1" aria-labelledby="modalKembalikanLabel<?= $book['id_buku']; ?>" aria-hidden="true">
+                                                <div class="modal fade" id="modalKembalikan<?= htmlspecialchars($book['id_buku']); ?>" tabindex="-1" aria-labelledby="modalKembalikanLabel<?= htmlspecialchars($book['id_buku']); ?>" aria-hidden="true">
                                                     <div class="modal-dialog">
                                                         <div class="modal-content">
                                                             <div class="modal-header">
-                                                                <h5 class="modal-title" id="modalKembalikanLabel<?= $book['id_buku']; ?>">Kembalikan Buku</h5>
+                                                                <h5 class="modal-title" id="modalKembalikanLabel<?= htmlspecialchars($book['id_buku']); ?>">Kembalikan Buku</h5>
                                                                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                                             </div>
                                                             <div class="modal-body">
-                                                                <p>Anda yakin ingin mengembalikan buku <strong><?= $book['judul_buku']; ?></strong>?</p>
+                                                                <p>Anda yakin ingin mengembalikan buku <strong><?= htmlspecialchars($book['judul_buku']); ?></strong>?</p>
                                                             </div>
                                                             <div class="modal-footer">
                                                                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
-                                                                <a href="function/kembalikan.php?id=<?= $book['id_buku']; ?>" class="btn btn-primary">Ya, Kembalikan</a>
+                                                                <a href="function/kembalikan.php?id=<?= htmlspecialchars($book['id_buku']); ?>" class="btn btn-primary">Ya, Kembalikan</a>
                                                             </div>
                                                         </div>
                                                     </div>
                                                 </div>
                                             </figcaption>
                                         </div>
-                                    </figcaption>
-                                </div>
-                            <?php endforeach; ?>
-                        </div><!-- grid -->
-                    </div><!-- product-list -->
-                </div><!-- inner-content -->
-            </div><!-- row -->
+                                    </div>
+                                <?php endforeach; ?>
+                            </div><!-- grid -->
+                        </div><!-- product-list -->
+                    </div><!-- inner-content -->
+                </div><!-- row -->
+            </div><!-- container -->
         </div><!-- container -->
     </section><!-- borrowed-books -->
 
-    <?php include 'footer.php'; ?> <!-- Pastikan path include file footer.php sudah benar -->
+    <?php include 'footer.php'; ?> <!-- Make sure the path to include the footer.php file is correct -->
 </body>
 
 </html>
